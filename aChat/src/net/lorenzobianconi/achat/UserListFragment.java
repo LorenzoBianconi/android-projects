aaf5d920567442736c006998a37250fe8e241584
lorenzo <me@lorenzobianconi.net>
2014-02-11 01:54:35 +0100

    Improve user list management of UserListFragment

--- aChat/src/net/lorenzobianconi/achat/UserListFragment.java
+++ aChat/src/net/lorenzobianconi/achat/UserListFragment.java
@@ -1,7 +1,10 @@
 package net.lorenzobianconi.achat;
 
 import java.util.ArrayList;
+
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,21 +12,51 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class UserListFragment extends Fragment {
+	private class UserListArrayAdapter extends ArrayAdapter<String> {
+		private Context _context = null;
+		private ArrayList<String> _list;
+		private int _layoutId;
+		
+		public UserListArrayAdapter(Context context, int resource,
+									ArrayList<String> objects) {
+			super(context, resource, objects);
+			_context = context;
+			_layoutId = resource;
+			_list = objects;
+		}
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View rowView;
+			LayoutInflater inflater = ((Activity)_context).getLayoutInflater();
+			rowView = inflater.inflate(_layoutId, parent, false);
+			
+			TextView textView = (TextView)rowView.findViewById(R.id.txtTitle);
+			String user = _list.get(position);
+			textView.setText(user);
+			if (user.equals(_parent._nick) == true)
+				textView.setTextColor(Color.GREEN);
+			return rowView;
+		}
+	}
+	
 	private AchatActivity _parent = null;
 	
 	private ListView _userListView = null;
 	private ArrayList<String> _userList = new ArrayList<String>();
-	private ArrayAdapter<String> _listAdapter = null;
+	private UserListArrayAdapter _listAdapter = null;
 	
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
     						 Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.user_list_fragment, container, false);
         
         _userListView = (ListView)view.findViewById(R.id.userListView);
-        _listAdapter = new ArrayAdapter<String>(getActivity(),
-        					android.R.layout.simple_list_item_1, _userList);        
+        _listAdapter = new UserListArrayAdapter(getActivity(), R.layout.listview_item_row,
+        										_userList);
+        LayoutInflater inflat = getActivity().getLayoutInflater();
+        View header = (View)inflat.inflate(R.layout.listview_header_row, null);
+        _userListView.addHeaderView(header);
         _userListView.setAdapter(_listAdapter);
         return view;
 	}
